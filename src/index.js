0d0addf8014069dc2d8964097030414d0ed53687
leloufadel <leloufadel@gmail.com>
2023-06-24 13:09:29 +0300

    update Js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+import populateScores from './modules/populateScores.js';
+import { submitScore } from './modules/domElements.js';
+
+submitScore.addEventListener('click', populateScores);
